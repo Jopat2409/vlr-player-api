f76b61ed0872253cda8552a8deb8445a446adede
Joe <joantpat@gmail.com>
2024-01-09 02:45:08 +0000

    Updated endpoints for new storage

--- app.py
+++ app.py
@@ -6,13 +6,11 @@ valorant.load_data()
 
 @app.route('/teams')
 def get_teams():
-    return jsonify(valorant.STATIC_DAT["tier1"]["teams"])
+    return jsonify(valorant.get_teams())
 
 @app.route('/team/<id>')
 def get_team(id):
-    data = valorant.team_from_id(int(id))
-    data.update({"players": valorant.players_from_team(int(id))})
-    return jsonify(data)
+    return jsonify(valorant.team_from_id(int(id)))
 
 @app.route('/player/<id>')
 def get_player(id):
